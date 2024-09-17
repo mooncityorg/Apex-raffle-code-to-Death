6cdd85940e3f77fbe3d3db2793cc804b3da64e0f
MoonCity <41557645+mooncityorg@users.noreply.github.com>
2024-09-17 23:14:41 +0900

    feat: fix raffle size and token mint address

--- src/config.tsx
+++ src/config.tsx
@@ -9,9 +9,9 @@ export const ADMINS = [
 export const GLOBAL_AUTHORITY_SEED = "global-authority";
 
 export const PROGRAM_ID = "FjZStEweLXQ7xkHHmQPchAkQ3zMz46b6JaWnoEdmSVRt";
-export const PREY_TOKEN_MINT = new PublicKey("nYDqQVEaQxLYLh8B8oAFXziMT1bcGrAVigZPL1s3dKc");
+export const PREY_TOKEN_MINT = new PublicKey("nYDqQVEaQxLYLhd8B8oAFXziMT1bcGrAVigZPL1s3dKc");
 // export const PREY_TOKEN_MINT = new PublicKey("2Dm1zu8ERJGBs3NLXt8s8Vor3YHwJye5E2pYhLiMHU4L");
 // nYDqQVEaQxLYLh8B8oAFXziMT1bcGrAVigZPL1s3dKc
 export const RAFFLE_SIZE = 66544;
-export const DECIMALS = 1000000000;
+export const DECIMALS = 10000000000;
 export const PREY_DECIMALS = 1000000000;
